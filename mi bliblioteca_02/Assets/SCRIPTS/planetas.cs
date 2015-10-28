40742747243328aeb21c790040e5ba21e68780c9
LC02-15 <LC02-15>
2015-10-28 15:40:53 -0600

    hice uno nuevo

---
+++ mi bliblioteca_02/Assets/SCRIPTS/planetas.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class planetas : MonoBehaviour {
+	public GameObject center;
+	public float speed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	//Permite girar un objeto al rededor de otro
+		//lo busque en google como rotate around unity
+		transform.RotateAround (new Vector3(0,10,0), Vector3.up, speed * Time.deltaTime);
+	}
+}
